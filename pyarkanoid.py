6f0137f244285b06d05b072e3b9b6a7191c40db2
Doyun Kim <doyunkim@fb.com>
2021-01-19 00:34:34 -0800

    update structure using sprite

--- pyarkanoid.py
+++ pyarkanoid.py
@@ -7,6 +7,90 @@
 import pygame
 import os
 import random
+
+current_path = os.path.dirname(__file__) # 현재 파일의 위치 반환
+image_path = os.path.join(current_path, "img") # 이미지 폴더 위치
+
+
+###############################################################
+# 클래스 선언
+class GameObj(pygame.sprite.Sprite):
+    def __init__(self, img_src, x, y):
+        super(GameObj, self).__init__()
+        # This needs to be updated when the item happens.
+        self.image = pygame.image.load(img_src)
+        self.rect = self.image.get_rect()
+        self.width = self.rect.size[0]
+        self.height = self.rect.size[1]
+        self.rect.x = x
+        self.rect.y = y
+        
+
+class Bar(GameObj):
+
+    img_src = os.path.join(image_path, "bar.png")
+
+    def __init__(self, x, y):
+        super(Bar, self).__init__(self.img_src, x, y)
+        # This needs to be updated when the item happens.
+        self.speed = 0.4
+        self.delta_x = 0
+        self.is_wide = False
+
+    def get_wide(self):
+        if not self.is_wide:
+            prev_x = self.rect.x
+            prev_y = self.rect.y
+            self.image = pygame.transform.scale(pygame.image.load(self.img_src), (self.width * 2, self.height))
+            self.is_wide = True
+            self.update_rect(prev_x, prev_y)
+
+    def get_narrow(self):
+        if self.is_wide:
+            prev_x = self.rect.x
+            prev_y = self.rect.y
+            self.image = pygame.transform.scale(pygame.image.load(self.img_src), (self.width * 1/2, self.height))
+            self.is_wide = False
+            self.update_rect(prev_x, prev_y)
+
+    def update_rect(self, x, y):
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.width = self.rect.size[0]
+        self.height = self.rect.size[1]
+
+
+class Ball(GameObj):
+
+    img_src = os.path.join(image_path, "ball.png")
+
+    def __init__(self, x, y):
+        super(Ball, self).__init__(self.img_src, x, y)
+        # This needs to be updated when the item happens.
+        self.speed_x = 0.2
+        self.speed_y = 0.2
+        self.max_speed = 0.5
+        self.delta_x = 0.0
+        self.delta_y = 0.0
+        self.is_penetrating = False
+
+
+class Block(GameObj):
+
+    img_src = os.path.join(image_path, "block_square.png")
+
+    def __init__(self, x, y):
+        super(Block, self).__init__(self.img_src, x, y)
+        # This needs to be updated when the item happens.
+        self.item = 0
+        self.rect.left = self.rect.x
+        self.rect.right = self.rect.x + self.width
+        self.rect.top = self.rect.y
+        self.rect.bottom = self.rect.y + self.height
+
+
+
 ###############################################################
 # 기본 초기화 (반드시 해야 하는 것들)
 pygame.init() # 초기화
@@ -27,8 +111,7 @@ clock = pygame.time.Clock()
 
 ###############################################################
 # 디렉토리 설정
-current_path = os.path.dirname(__file__) # 현재 파일의 위치 반환
-image_path = os.path.join(current_path, "img") # 이미지 폴더 위치
+
 
 # 배경 만들기
 background = pygame.image.load(os.path.join(image_path, "background.png"))
@@ -36,34 +119,24 @@ background = pygame.image.load(os.path.join(image_path, "background.png"))
 # 가상의 stage
 stage_height = 50 # 막대를 화면 바닥보다 50만큼 띄워줄 것이다.
 
-# (움직이는) 막대 만들기
-bar = pygame.image.load(os.path.join(image_path, "bar.png"))
-bar_size = bar.get_rect().size
-bar_width = bar_size[0]
-bar_height = bar_size[1]
-bar_x_pos = (screen_width / 2 ) - (bar_width / 2)
-bar_y_pos = screen_height - stage_height - bar_height
+# 막대 만들기
+bar = Bar(0, 0)
+bar.rect.x = (screen_width / 2 ) - (bar.width / 2)
+bar.rect.y = screen_height - stage_height - bar.height
 
-# 막대 움직임 제어
-bar_to_x = 0
-bar_speed = 0.4
+# 공 만들기
+ball = Ball(0, 0)
+ball.rect.x = (screen_width / 2 ) - (ball.width / 2)
+ball.rect.y = screen_height - stage_height - bar.height - ball.height
 
-# 공 만들기 (5x5 size)
-ball = pygame.image.load(os.path.join(image_path, "ball.png"))
-ball_size = ball.get_rect().size
-ball_width = ball_size[0]
-ball_height = ball_size[1]
-ball_x_pos = (screen_width / 2 ) - (ball_width / 2)
-ball_y_pos = screen_height - stage_height - bar_height - ball_height
+# 막대와 공을 같이 관리
+sprites = pygame.sprite.Group()
+sprites.add(bar)
+sprites.add(ball)
 
-# 공 움직임 제어
-ball_speed_x = 0.2
-ball_speed_y = 0.2 
-ball_max_speed_x = 0.5
-ball_max_speed_y = 0.5
+# 벽돌 그룹 만들기
+blocks = pygame.sprite.Group()
 
-# 벽돌들
-blocks = []
 block_rows = 5
 block_cols = 10
 blocks_to_check = []
@@ -81,10 +154,8 @@ for i in range(block_rows):
         block_y_pos = 50 + i * block_height
 
         # 벽돌들은 필요한 정보가 위치 정보 뿐이므로 위치 정보를 등록시킨다.
-        blocks.append({
-            "block_x_pos": block_x_pos,
-            "block_y_pos": block_y_pos,
-        })
+        block = Block(block_x_pos, block_y_pos)
+        blocks.add(block)
 
 # 폰트 정의
 game_font = pygame.font.Font(None, 40)
@@ -118,40 +189,40 @@ while running:
         # 키보드 이벤트 처리 : 막대의 좌우 움직임만 처리하면 된다.
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                bar_to_x -= bar_speed
+                bar.delta_x -= bar.speed
             elif event.key == pygame.K_RIGHT:
-                bar_to_x += bar_speed
+                bar.delta_x += bar.speed
 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                bar_to_x = 0    
+                bar.delta_x = 0    
 
     # 바 위치 변경 (dt를 곱해서 FPS에 맞게 속도를 맞춰준다)
-    bar_x_pos += bar_to_x * dt
+    bar.rect.x += bar.delta_x * dt
     
-    if bar_x_pos < 0:
-        bar_x_pos = 0
-    elif bar_x_pos > screen_width - bar_width:
-        bar_x_pos = screen_width - bar_width
+    if bar.rect.x < 0:
+        bar.rect.x = 0
+    elif bar.rect.x > screen_width - bar.width:
+        bar.rect.x = screen_width - bar.width
 
 
     # 공의 위치 및 움직임 
-    ball_x_pos += ball_speed_x * dt
-    ball_y_pos += ball_speed_y * dt
+    ball.rect.x += ball.speed_x * dt
+    ball.rect.y += ball.speed_y * dt
 
     # 화면의 가장자리(위, 오른쪽, 왼쪽)와 충돌 했을 경우에 공을 튕겨낸다.    
-    if ball_x_pos < 0:
-        ball_x_pos = 0
-        ball_speed_x = -ball_speed_x
-    elif ball_x_pos > screen_width - ball_width:
-        ball_x_pos = screen_width - ball_width
-        ball_speed_x = -ball_speed_x
-    if ball_y_pos < 0:
-        ball_y_pos = 0
-        ball_speed_y = -ball_speed_y
+    if ball.rect.x < 0:
+        ball.rect.x = 0
+        ball.speed_x = -ball.speed_x
+    elif ball.rect.x > screen_width - ball.width:
+        ball.rect.x = screen_width - ball.width
+        ball.speed_x = -ball.speed_x
+    if ball.rect.y < 0:
+        ball.rect.y = 0
+        ball.speed_y = -ball.speed_y
     # 볼이 화면 아래로 나가는 경우: 게임오버
-    elif ball_y_pos > screen_height - ball_height:
-        ball_y_pos = screen_height - ball_height
+    elif ball.rect.y > screen_height - ball.height:
+        ball.rect.y = screen_height - ball.height
         game_result = "Game Over"
         running = False
 ###############################################################
@@ -162,60 +233,50 @@ while running:
     # 충돌 처리
 
     # 막대 정보 읽어오기
-    bar_rect = bar.get_rect()
-    bar_rect.left = bar_x_pos
-    bar_rect.top = bar_y_pos
+    # bar_rect = bar.get_rect()
+    bar.rect.left = bar.rect.x
+    bar.rect.top = bar.rect.y
     
     # 공 정보 읽어오기
-    ball_rect = ball.get_rect()
-    ball_rect.left = ball_x_pos
-    ball_rect.right = ball_x_pos + ball_rect.size[0]
-    ball_rect.top = ball_y_pos
-    ball_rect.bottom = ball_y_pos + ball_rect.size[1]
+    # ball.rect = ball.get_rect()
+    ball.rect.left = ball.rect.x
+    ball.rect.right = ball.rect.x + ball.rect.size[0]
+    ball.rect.top = ball.rect.y
+    ball.rect.bottom = ball.rect.y + ball.rect.size[1]
 
     # 블럭 정보 읽어오고 공과 충돌 처리
-    for block_idx, block_val in enumerate(blocks):
-        block_pos_x = block_val["block_x_pos"]
-        block_pos_y = block_val["block_y_pos"]
-
-        block_rect = block_img.get_rect()
-        block_rect.left = block_pos_x
-        block_rect.right = block_pos_x + block_rect.size[0]
-        block_rect.top = block_pos_y
-        block_rect.bottom = block_pos_y + block_rect.size[1]
+    for block_idx, block in enumerate(blocks.sprites()):
 
         # 볼과 블럭의 충돌을 처리한다.
         # 일단 충돌했는지 파악하고, 충돌했으면 옆면으로 부딪혔는지, 위아래 면으로 부딪혔는지 파악해서
 
         # 공이 충돌했을 때 valid한 충돌인지 계산
         # 공을 한번에 하나씩만 지울것이라 for loop 밖에서 돌것이다.
-        if ball_rect.colliderect(block_rect):
-            left_overlap = ball_rect.right - block_rect.left
-            right_overlap = block_rect.right - ball_rect.left
-            top_overlap = ball_rect.bottom - block_rect.top
-            bottom_overlap = block_rect.bottom - ball_rect.top
+        if ball.rect.colliderect(block.rect):
+            left_overlap = ball.rect.right - block.rect.left
+            right_overlap = block.rect.right - ball.rect.left
+            top_overlap = ball.rect.bottom - block.rect.top
+            bottom_overlap = block.rect.bottom - ball.rect.top
             
             collision_side_list = []
             # 공의 타당한 충돌방향과 충돌 후 지난 시간을 구해준다.
-            if left_overlap > 0 and ball_speed_x > 0:
+            if left_overlap > 0 and ball.speed_x > 0:
                 direction = 'horizontal'
-                t_after_col = left_overlap / abs(ball_speed_x)
+                t_after_col = left_overlap / abs(ball.speed_x)
                 collision_side_list.append((direction, t_after_col))
-            if right_overlap > 0 and ball_speed_x < 0:
+            if right_overlap > 0 and ball.speed_x < 0:
                 direction = 'horizontal'
-                t_after_col = right_overlap / abs(ball_speed_x)
+                t_after_col = right_overlap / abs(ball.speed_x)
                 collision_side_list.append((direction, t_after_col))
-            if top_overlap > 0 and ball_speed_y > 0:
+            if top_overlap > 0 and ball.speed_y > 0:
                 direction = 'vertical'
-                t_after_col = top_overlap / abs(ball_speed_y)
+                t_after_col = top_overlap / abs(ball.speed_y)
                 collision_side_list.append((direction, t_after_col))
-            if bottom_overlap > 0 and ball_speed_y < 0:
+            if bottom_overlap > 0 and ball.speed_y < 0:
                 direction = 'vertical'
-                t_after_col = bottom_overlap / abs(ball_speed_y)
+                t_after_col = bottom_overlap / abs(ball.speed_y)
                 collision_side_list.append((direction, t_after_col))
             
-            print(collision_side_list)
-            print(ball_speed_x, ball_speed_y)
             # 충돌 후 시간이 가장 큰 것을 구해서 가장 먼저 충돌했을 면을 구한다.
             min_direction = None
             min_t = 0
@@ -243,44 +304,31 @@ while running:
     
     # 충돌한 블럭은 지워준다.
     if min_idx >= 0:
-        # 위치를 딱 충돌할 때로 되돌려 놓는다.
-        # ball_x_pos = ball_x_pos - (ball_speed_x * max_t)
-        # ball_y_pos = ball_y_pos - (ball_speed_x * max_t)
+        
         # 필요한 속도를 반대 방향으로 바꾸어 준다.
-        print(min_side)
         if min_side == 'horizontal':
-            ball_speed_x = -ball_speed_x
+            ball.speed_x = -ball.speed_x
         else:
-            ball_speed_y = -ball_speed_y
-        del blocks[min_idx]
+            ball.speed_y = -ball.speed_y
+        blocks_tocheck = blocks.sprites()
+        block_to_remove = blocks_tocheck[min_idx]
+        blocks.remove(block_to_remove)
     blocks_to_check = []
 
     # 모든 블럭을 다 없앨 경우 성공
-    if len(blocks) == 0:
+    if len(blocks.sprites()) == 0:
         running = False
         game_result = "Mission Complete"
 
     # 공과 바가 충돌했을 때. y축 속도를 바꾸고, x축 속도도 랜덤하게 바꾼다.
-    if ball_rect.colliderect(bar_rect):
-        ball_speed_y = -(ball_speed_y)
-        # new_ball_speed_x = ball_speed_x + random.uniform(-0.05, 0.05) * dt
-        # if new_ball_speed_x * ball_speed_x > 0:
-        #     ball_speed_x = new_ball_speed_x
+    if ball.rect.colliderect(bar.rect):
+        ball.speed_y = -(ball.speed_y)
+        
 ###############################################################
 
 
     
 ###############################################################
-    # 화면에 그리기
-    screen.blit(background, (0, 0))
-    screen.blit(bar, (bar_x_pos, bar_y_pos))
-    screen.blit(ball, (ball_x_pos, ball_y_pos))
-    
-    for idx, val in enumerate(blocks):
-        block_x_pos = val["block_x_pos"]
-        block_y_pos = val["block_y_pos"]
-        screen.blit(block_img, (block_x_pos, block_y_pos))
-
     # 경과 시간 계산
     elapsed_time = (pygame.time.get_ticks() - start_ticks) / 1000
     timer = game_font.render("Time : {}".format(int(total_time - elapsed_time)), True, (255, 255, 255))
@@ -288,7 +336,11 @@ while running:
     if total_time - elapsed_time <= 0:
         game_result = "Time Over"
         running = False
-
+    
+    # 화면에 그리기
+    screen.blit(background, (0, 0))
+    blocks.draw(screen)
+    sprites.draw(screen)
     screen.blit(timer, (10, 10))
 
     pygame.display.update() # 게임 화면을 다시 그리기 
